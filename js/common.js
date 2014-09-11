e96114f6146c8a85662e13be8d3a13892309f1c9
Georgiy <goshazvir@gmail.com>
2014-09-11 20:40:57 +0300

    done

--- js/common.js
+++ js/common.js
@@ -47,6 +47,41 @@ $(document).ready(function() {
 		});
 	} modals();
 
+	function select() {
+		var el = $('.js-select');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).addClass('is-selected').siblings().removeClass('is-selected');
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	$(document).click(function() {
+		$('.js-select').removeClass('is-open');
+	});
+
 });
 
 
